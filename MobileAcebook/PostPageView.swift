7c85cb08e71fcf8625f0dde6cb688c838f1927e8
siwhelan <simon@swhelan.dev>
2024-02-22 18:10:48 +0000

    fixed username bug on feed page

--- MobileAcebook/PostPageView.swift
+++ MobileAcebook/PostPageView.swift
@@ -8,34 +8,58 @@
 import Foundation
 import SwiftUI
 
+
+// When you scroll, SwiftUI reuses views for efficiency, 
+// and the .onAppear() modifier is called each time a view comes into view.
+// Since postOwnerModel is a shared instance across all list items, updating it in
+//  .onAppear for one post causes all visible post views to update their postOwner to the last fetched user.
+
+// Fix :
+
+// Assign each post it's username directly first
+struct PostView: View {
+    let post: Post
+    @ObservedObject var postOwnerModel: PostUser = PostUser() // name tag for the post.
+    
+    init(post: Post) {
+        self.post = post
+        // when we create the name tag, we write the name of the post's author on it.
+        self.postOwnerModel.fetchPostUser(userId: post.createdBy)
+    }
+    
+    var body: some View {
+        VStack(alignment: .leading) {
+            Text(post.message) // The content of the post.
+            
+            HStack {
+                Text(postOwnerModel.postOwner?.username ?? "Loading...") // The name on the name tag.
+                Spacer()
+                Text(convertDateFormat(inputDate: post.createdAt))
+            }
+        }
+    }
+}
+
+
+
+// This is the view where all posts are
 struct PostPageView: View {
-    @ObservedObject var postModel = PostsView()
-    @ObservedObject var postOwnerModel = PostUser()
+    @ObservedObject var postModel = PostsView() // This holds all the posts.
+    
     var body: some View {
         VStack {
             Text("Posts Feed")
                 .font(.title)
-                .fontWeight(/*@START_MENU_TOKEN@*/.bold/*@END_MENU_TOKEN@*/)
+                .fontWeight(.bold)
             List(postModel.posts) { post in
-                Text(post.message)
-                
-                HStack{
-                    Text(postOwnerModel.postOwner?.username ?? "")
-                        .font(.footnote)
-                        .onAppear(){
-                            postOwnerModel.fetchPostUser(userId: post.createdBy)
-                        }
-                    Spacer()
-                    Text(convertDateFormat(inputDate:
-                                            post.createdAt))
-                }                           .font(.footnote)
+                PostView(post: post) // Here we add each post with its name tag on the view.
             }
         }
-            .onAppear {
-                postModel.fetchPosts()
-            }
+        .onAppear {
+            postModel.fetchPosts() // load the full collection at once
         }
     }
+}
 
 struct PostPageView_Previews: PreviewProvider {
     static var previews: some View {
--- MobileAcebook/Services/GetPostsService.swift
+++ MobileAcebook/Services/GetPostsService.swift
@@ -46,19 +46,22 @@ class PostUser: ObservableObject {
     
     func fetchPostUser(userId: String) {
         if let url = URL(string: "http://127.0.0.1:8080/posts/\(userId)") {
-          var request = URLRequest(url: url)
-          request.httpMethod = "GET"
-          request.setValue("application/json", forHTTPHeaderField: "Content-Type")
+            var request = URLRequest(url: url)
+            request.httpMethod = "GET"
+            request.setValue("application/json", forHTTPHeaderField: "Content-Type")
             request.setValue("Bearer \(token ?? "")", forHTTPHeaderField: "Authorization")
             request.addValue("application/json", forHTTPHeaderField: "Accept")
-          URLSession.shared.dataTask(with: request) { data, response, error in
-              if let data = data, let result = try? JSONDecoder().decode(UserData.self, from:data) {
-                  self.postOwner = result.ownerData
-                  print(result.ownerData)
-              }
-          }.resume()
+            
+            URLSession.shared.dataTask(with: request) { data, response, error in
+                if let data = data, let result = try? JSONDecoder().decode(UserData.self, from: data) {
+                    DispatchQueue.main.async {
+                        self.postOwner = result.ownerData
+                        print(result.ownerData)
+                    }
+                }
+            }.resume()
         } else {
-          print("Invalid URL")
+            print("Invalid URL")
         }
     }
 }
