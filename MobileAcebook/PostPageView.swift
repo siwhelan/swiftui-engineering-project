85a50385929ee20130f721bb3d631a62d1e92919
“Leah” <lnsimonn@hotmail.com>
2024-02-22 14:23:22 +0000

    implement timestamps and username details

--- MobileAcebook/PostPageView.swift
+++ MobileAcebook/PostPageView.swift
@@ -10,19 +10,33 @@ import SwiftUI
 
 struct PostPageView: View {
     @ObservedObject var postModel = PostsView()
+    @ObservedObject var postOwnerModel = PostUser()
     var body: some View {
         VStack {
-            Text("Posts Feed").font(.title)
+            Text("Posts Feed")
+                .font(.title)
                 .fontWeight(/*@START_MENU_TOKEN@*/.bold/*@END_MENU_TOKEN@*/)
             List(postModel.posts) { post in
                 Text(post.message)
+                
+                HStack{
+                    Text(postOwnerModel.postOwner?.username ?? "")
+                        .font(.footnote)
+                        .onAppear(){
+                            postOwnerModel.fetchPostUser(userId: post.createdBy)
+                        }
+                    Spacer()
+                    Text(convertDateFormat(inputDate:
+                                            post.createdAt))
+                }                           .font(.footnote)
             }
+        }
             .onAppear {
                 postModel.fetchPosts()
             }
         }
     }
-}
+
 struct PostPageView_Previews: PreviewProvider {
     static var previews: some View {
         PostPageView()
