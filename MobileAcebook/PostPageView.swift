42cf5588ff1bbba05acf38df5fde6c9749f291e6
“Leah” <lnsimonn@hotmail.com>
2024-02-22 13:31:00 +0000

    implement posts page feed

--- MobileAcebook/PostPageView.swift
+++ MobileAcebook/PostPageView.swift
@@ -8,9 +8,23 @@
 import Foundation
 import SwiftUI
 
-
 struct PostPageView: View {
+    @ObservedObject var postModel = PostsView()
     var body: some View {
-        Text("Posts Feed")
+        VStack {
+            Text("Posts Feed").font(.title)
+                .fontWeight(/*@START_MENU_TOKEN@*/.bold/*@END_MENU_TOKEN@*/)
+            List(postModel.posts) { post in
+                Text(post.message)
+            }
+            .onAppear {
+                postModel.fetchPosts()
+            }
+        }
+    }
+}
+struct PostPageView_Previews: PreviewProvider {
+    static var previews: some View {
+        PostPageView()
     }
 }
